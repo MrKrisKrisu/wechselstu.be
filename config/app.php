0d6944b6698d06b9a199f7245a475ade37068463
Kristian Stöckel <git@k118.de>
2025-06-18 14:51:08 +0200

    change app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Kassenwächter'),
+    'name' => env('APP_NAME', 'wechselstu.be'),
 
     /*
     |--------------------------------------------------------------------------
