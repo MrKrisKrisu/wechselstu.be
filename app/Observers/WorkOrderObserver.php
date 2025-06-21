aba5832f1af8b1e66eae75cac9d4d01eb39b3220
Kristian Stöckel <git@k118.de>
2025-06-21 18:11:31 +0200

    don't delete messages in matrix if done

--- app/Observers/WorkOrderObserver.php
+++ app/Observers/WorkOrderObserver.php
@@ -2,7 +2,6 @@
 
 namespace App\Observers;
 
-use App\Enum\WorkOrderStatus;
 use App\Http\Controllers\PublicWorkOrderController;
 use App\Models\WorkOrder;
 use App\Services\MatrixService;
@@ -11,10 +10,6 @@ class WorkOrderObserver {
     public function updated(WorkOrder $workOrder): void {
         if($workOrder->wasChanged('status') && $workOrder->event_id) {
             $matrix = new MatrixService();
-            if($workOrder->status === WorkOrderStatus::DONE) {
-                $matrix->deleteMessage($workOrder->event_id, 'done');
-                return;
-            }
             $matrix->updateMessage($workOrder->event_id, PublicWorkOrderController::getMessageForWorkOrder($workOrder));
         }
     }
